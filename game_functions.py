d55df12af8431abeb9649b56636fffb8b9dba2f6
Randy Polanco <70306858+randyp03@users.noreply.github.com>
2023-10-30 01:29:18 -0400

    Added natural hands, and insurance opportunties

--- game_functions.py
+++ game_functions.py
@@ -4,55 +4,96 @@ import random
 card_categories = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 card_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-def start_game(player_list: list, dealt_cards: dict):
+def start_game():
     # shuffles the deck
     deck = [(card, category) for category in card_categories for card in card_list]
     random.shuffle(deck)
 
     # while loop to get all players
+    sign_up_list = []
     add_player = True
+
     while add_player:
         name = input('Enter your name: ')
-        player_list.append(name)
+        sign_up_list.append(name)
         
-        answer = input('\nIs there another player (y/n): ')
+        answer = input('\nIs there another player (y/n)? ')
         if answer.lower().startswith('y'):
             print()
             continue
         else:
             add_player = False
-    print(f'\nNumber of players: {len(player_list)}\n')
+    print(f'\nNumber of players: {len(sign_up_list)}')
 
-    # sets the initial deal to everyone
-    for player in player_list:
-        dealt_cards[player] = []
-    dealt_cards['Dealer'] = []
-    for i in range(2):
-        for person in dealt_cards:
-            dealt_cards[person].append(deck.pop(0))
+
+    # creating dict objects for each player
+    players = []
+    for player in sign_up_list:
+            players.append(
+                {
+                    "Name": player,
+                    "Hand": [],
+                    "Hand2": [],
+                    "Natural": False,
+                    "Insurance": False,
+                    "Split": False,
+                    "DoubleDown": False,
+                    "Surrender": False
+                }
+            )
     
-    return player_list, dealt_cards, deck
+    # creating a dealer object
+    players.append(
+        {
+            "Name": "Dealer",
+            "Hand": [],
+            "Natural": False
+        }
+    )
+    
+    # dealing two cards to each player and dealer
+    for i in range(2):
+        for person in players:
+            person["Hand"].append(deck.pop(0))
+    
+    return players, deck
+
+# checks dealer's up card for possible insurance or natural blackjack
+def check_face_up(player):
+    if player["Hand"][0][0] == "A":
+        return "A"
+    elif player["Hand"][0][0] in ['10', 'J', 'Q', 'K']:
+        return "ten-card"
+    else:
+        return False
+
+# checks if the current player is a dealer
+def isDealer(player):
+    if player["Name"] == 'Dealer':
+        return True
+    else:
+        return False
 
 # revealing deals
-def display_deal(dealt_cards: dict, player, reveal=False):
-    if player != 'Dealer':
-        for card in dealt_cards[player]:
+def display_hand(player, reveal=False):
+    if not isDealer(player):
+        for card in player['Hand']:
             print(card)
     else:
         if reveal:
             print('\nDealer\'s cards are: ')
-            for card in dealt_cards['Dealer']:
+            for card in player['Hand']:
                 print(card)
         else:
-            print(f"Dealer's cards:\n{dealt_cards['Dealer'][0]}\nUnknown")
+            print(f"\nDealer's cards:\n{player['Hand'][0]}\nUnknown")
 
 # deals extra card
-def extra_deal(dealt_cards: dict, player, deck: list):
-    dealt_cards[player].append(deck.pop(0))
+def hit(player, deck: list):
+    player["Hand"].append(deck.pop(0))
 
 # get total value of deal
-def get_total(dealt_cards: dict, player):
-    card_list = [card[0] for card in dealt_cards[player]]
+def get_total(player):
+    card_list = [card[0] for card in player["Hand"]]
     # ace moved to last to better sum total of deal and determine soft value
     if 'A' in card_list:
         card_list.append(card_list.pop(card_list.index('A')))
@@ -61,13 +102,13 @@ def get_total(dealt_cards: dict, player):
         # A = 1 or 11
         if card == 'A':
             # for dealers if an 11 Ace gets them a bust deal, then Ace becomes 1
-            if player == 'Dealer' and total + 11 > 21:
+            if isDealer(player) and total + 11 > 21:
                 total += 1
-            elif player == 'Dealer' and 17 <= total + 11 <= 21:
+            elif isDealer(player) and 17 <= total + 11 <= 21:
                 total += 11
             # ace will always be 11 unless the total value is over 21,
             # then treated as 1
-            elif player != 'Dealer' and total + 11 > 21:
+            elif not isDealer(player) and total + 11 > 21:
                 total += 1
             else:
                 total+= 11
@@ -80,29 +121,51 @@ def get_total(dealt_cards: dict, player):
     return total
 
 # dealer takes his turn
-def dealer_turn(dealt_cards:dict, deck:list):
-    while True:
-        display_deal(dealt_cards,'Dealer', reveal=True)
-        if get_total(dealt_cards, 'Dealer') < 17:
-            extra_deal(dealt_cards, 'Dealer', deck)
-        elif 17<= get_total(dealt_cards, 'Dealer') <= 21:
-            break
-        else:
-            break
+def dealer_turn(dealer, deck:list):
+    if dealer["Natural"]:
+        return
+    else:
+        while True:
+            if get_total(dealer) < 17:
+                hit(dealer, deck)
+            elif 17<= get_total(dealer) <= 21:
+                if get_total(dealer) == 21:
+                    print('\nDealer drew a blackjack!')
+                    break
+                else:
+                    break
+            else:
+                break
+        display_hand(dealer, reveal=True)
 
 # shows the results of the cards dealt to each person
-def get_result(player_list: list, dealt_cards:dict):
-    for player in player_list:
-        if get_total(dealt_cards, player) > 21:
+def get_result(players):
+    for player in players:
+        if isDealer(player):
+            break
+        elif player["Surrender"]:
+            print(f'{player["Name"]} surrendered.')
+        # if the current player already lost by drawing a bust
+        elif get_total(player) > 21:
             continue
-        elif get_total(dealt_cards, player) < 21 and get_total(dealt_cards, 'Dealer') > 21:
-            print(f'\n{player}, you won! The dealer dealt a bust.')
-        elif get_total(dealt_cards, player) > get_total(dealt_cards, 'Dealer'):
-            print(f'\n{player}, you won! You dealt higher than the dealer.')
-        elif get_total(dealt_cards, player) == get_total(dealt_cards, 'Dealer'):
-            print(f'\n{player}, you drawed with the dealer.')
-        elif get_total(dealt_cards, player) < get_total(dealt_cards, 'Dealer'):
-            print(f'\n{player}, you lose. You dealt lower than the dealer')
+        # if the dealer has a natural blackjack
+        elif players[-1]["Natural"]:
+            continue
+        # if dealer doesn't have natural blackjack
+        elif not players[-1]["Natural"]:
+            # rewards player if they have a natural blackjack
+            if player["Natural"]:
+                continue
+            # compares results of player's hand vs dealer's hand
+            else:
+                if get_total(players[-1]) > 21:
+                    print(f'\n{player["Name"]}, you won! The dealer dealt a bust.')
+                elif get_total(player) > get_total(players[-1]):
+                    print(f'\n{player["Name"]}, you won! You dealt higher than the dealer.')
+                elif get_total(player) == get_total(players[-1]):
+                    print(f'\n{player["Name"]}, you drawed with the dealer.')
+                elif get_total(player) < get_total(players[-1]):
+                    print(f'\n{player["Name"]}, you lose. You dealt lower than the dealer')
         
-        print(f'Your deal total: {get_total(dealt_cards, player)}')
-        print(f'Dealer\'s deal total: {get_total(dealt_cards, "Dealer")}')
+                print(f'Your deal total: {get_total(player)}')
+                print(f'Dealer\'s deal total: {get_total(players[-1])}')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Blackjack
 
-import game_functions
+import game_functions, json
 
 running = True
 
@@ -9,43 +9,98 @@ print()
 
 # main game
 while running:
-    player_list = []
-    dealt_cards = {}
     
-    player_list, dealt_cards, deck = game_functions.start_game(player_list, dealt_cards)
+    # starts the game by creating players, and dealing cards
+    players, deck = game_functions.start_game()
+
+    # check if possibility of insurance
+    if game_functions.check_face_up(players[-1]) == 'A':
+        game_functions.display_hand(players[-1])
+        print("\nDealer has an ace up. Dealer has opened the option for insurance.")
+        for player in players:
+            if game_functions.isDealer(player):
+                break
+            else:
+                has_insurance = input(f'\n{player["Name"]}, do you want to place an insurance bet (y/n)? ')
+                if has_insurance.lower().startswith('y'):
+                    player["Insurance"] = True
+                    print(f'\n{player["Name"]} has placed insurance')
+
+    # check if dealer has a natural blackjack
+    if game_functions.check_face_up(players[-1]) == 'A' or game_functions.check_face_up(players[-1]) == 'ten-card':
+        if game_functions.get_total(players[-1]) == 21:
+            print("\nDealer has natural blackjack!")
+            players[-1]["Natural"] = True
+        # check if any players have made insurance bets
+        for player in players:
+            if game_functions.isDealer(player):
+                break
+            elif player["Insurance"] and players[-1]["Natural"]:
+                print(f'\n{player["Name"]}, your decision for an insurance bet was lucky')
+            elif player["Insurance"] and not players[-1]["Natural"]:
+                print(f'\n{player["Name"]}, your insurance bet was good for nothing. Dummy.')
+            else:
+                continue
 
     # each player takes their turn before dealer
-    for player in player_list:
-        game_functions.display_deal(dealt_cards, 'Dealer')
-        print(f'\n{player}, here is your hand:')
-        game_functions.display_deal(dealt_cards, player)
-
-        # checks if the player's hand is a natural
-        if game_functions.get_total(dealt_cards, player) == 21:
-            print('You got blackjack!')
+    for player in players:
+        if game_functions.isDealer(player):
             break
-        else: 
-            while True:
-                option = input('\nDo you want to stand or hit? ')
-                if option.lower() == 'stand':
-                    break
-                elif option.lower() == 'hit':
-                    game_functions.extra_deal(dealt_cards, player, deck)
-                    print(f'\n{player}, here is your new hand:')
-                    for card in dealt_cards[player]:
-                        print(card)
-                    # Automatically end the player's turn if their hand is a bust
-                    if game_functions.get_total(dealt_cards,player) > 21:
-                        print('\nBusted. You lose.')
+        # if dealer has natural hand, immediately check if players also have natural hand
+        elif players[-1]["Natural"]:
+            # displaying dealer's full hand
+            print(f'\nHere is the dealer\'s hand')
+            game_functions.display_hand(players[-1], True)
+            # displaying current player's hand
+            print(f'\n{player["Name"]}, here is your hand:')
+            game_functions.display_hand(player)
+            # checking whether player also has natural hand
+            if game_functions.get_total(player) == 21:
+                player["Natural"] = True
+                print(f'\n{player["Name"]}, You also got natural blackjack! Lucky you.')
+                continue
+            else:
+                print(f'\n{player["Name"]}, You don\'t have a natural hand. Tough luck.')
+                continue
+        # if not dealer, and dealer doesn't have natural hand, proceed normally
+        else:
+            # display dealer's hand
+            game_functions.display_hand(players[-1])
+            # displaying current player's hand
+            print(f'\n{player["Name"]}, here is your hand:')
+            game_functions.display_hand(player)
+
+            # checks if the player's hand is a natural blackjack
+            if game_functions.get_total(player) == 21:
+                player["Natural"] = True
+                print(f'\n{player["Name"]}, you have a natural hand, and the dealer doesn\'t. You win!')
+                continue
+            else: 
+                while True:
+                    option = input('\nDo you want to stand or hit? ')
+                    if option.lower() == 'stand':
                         break
+                    elif option.lower() == 'hit':
+                        game_functions.hit(player, deck)
+                        print(f'\n{player["Name"]}, here is your new hand:')
+                        for card in player["Hand"]:
+                            print(card)
+                        # Automatically end the player's turn if they draw a blackjack or bust
+                        if game_functions.get_total(player) == 21:
+                            print('\nYou got blackjack!')
+                            break
+                        elif game_functions.get_total(player) > 21:
+                            print('\nBusted. You lose.')
+                            break
+                        else:
+                            continue
                     else:
-                        continue
-                else:
-                    print('\nThat is not one of the options. Please try again.')
+                        print('\nThat is not one of the options. Please try again.')
 
-    game_functions.dealer_turn(dealt_cards, deck)
-
-    game_functions.get_result(player_list,dealt_cards)
+    # dealer has their turn
+    game_functions.dealer_turn(players[-1], deck)
+    # gets result of comparing the player vs the dealer
+    game_functions.get_result(players)
     
     while True:
         play_again = input('\nDo you want to play again? (y/n)')
